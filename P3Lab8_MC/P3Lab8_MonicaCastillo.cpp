3a29cbff0af08f362fb5c8b2d94e07cd4872d573
monicastle <monicastillo@unitec.edu>
2020-09-11 17:16:19 -0600

    Fin del Laboratorio

--- P3Lab8_MC/P3Lab8_MonicaCastillo.cpp
+++ P3Lab8_MC/P3Lab8_MonicaCastillo.cpp
@@ -33,15 +33,37 @@ int main(){
     box(ventanaBox2, 0, 0);
     box(ventanaBox3, 0, 0);
     //start_color();
-    init_pair(2, COLOR_YELLOW, COLOR_CYAN);
+    init_pair(2, COLOR_BLACK, COLOR_CYAN);
     attrset(COLOR_PAIR(2));
-    //string software = "Software";
+    // Imprimir Texto de la Ventana Principal
+    wattron(ventanaPrincipal, COLOR_PAIR(2));
     mvwprintw(ventanaPrincipal, 0, 1, "YaST @ g185");
+    mvwprintw(ventanaPrincipal, 0, 0, " ");
+    for (int i = 12; i < MaxX - 18; i++){
+        mvwprintw(ventanaPrincipal, 0, i, " ");
+    } // Fin For  
     mvwprintw(ventanaPrincipal, 0, MaxX - 18, "Press F1 for Help");
-    mvwprintw(ventanaPrincipal, MaxY - 2, 2, "[Help]");
-    mvwprintw(ventanaPrincipal, MaxY - 2, MaxX - 8, "[Quit]");
+    mvwprintw(ventanaPrincipal, 0, MaxX, " ");
+    wattroff(ventanaPrincipal, COLOR_PAIR(2));
+    mvwprintw(ventanaPrincipal, MaxY - 2, 2, "[");
+    mvwprintw(ventanaPrincipal, MaxY - 2, MaxX - 8, "[");
+    init_pair(3, COLOR_YELLOW, COLOR_BLUE);
+    attrset(COLOR_PAIR(3));
+    wattron(ventanaPrincipal, COLOR_PAIR(3));
+    mvwprintw(ventanaPrincipal, MaxY - 2, 3, "H");
+    mvwprintw(ventanaPrincipal, MaxY - 2, MaxX - 7, "Q");
+    wattroff(ventanaPrincipal, COLOR_PAIR(3));
+    mvwprintw(ventanaPrincipal, MaxY - 2, 4, "elp]");
+    mvwprintw(ventanaPrincipal, MaxY - 2, MaxX - 6, "uit]");
     // Imprimir Texto de la Primera Caja
+    init_pair(6, COLOR_YELLOW, COLOR_CYAN);
+    attrset(COLOR_PAIR(6));
+    wattron(ventanaBox1, COLOR_PAIR(6)); 
     mvwprintw(ventanaBox1, 1, 1, "Software");
+    for (int i = 9; i < (MaxX / 3) - 3; i++){
+        mvwprintw(ventanaBox1, 1, i, " ");
+    }
+    wattroff(ventanaBox1, COLOR_PAIR(6));
     mvwprintw(ventanaBox1, 2, 1, "Hardware");
     mvwprintw(ventanaBox1, 3, 1, "System");
     mvwprintw(ventanaBox1, 4, 1, "Network Devices");
@@ -49,10 +71,17 @@ int main(){
     mvwprintw(ventanaBox1, 6, 1, "Security and Users");
     mvwprintw(ventanaBox1, 7, 1, "Misc");
     // Imprimir Texto de la Segunda Caja
-
+    init_pair(4, COLOR_YELLOW, COLOR_BLUE);
+    attrset(COLOR_PAIR(4));
+    wattron(ventanaBox2, COLOR_PAIR(4));   
     mvwprintw(ventanaBox2, 1, 1, "YaSTZ Control Center");
+    wattroff(ventanaBox2, COLOR_PAIR(4));
     // Imprimir Texto de la Tercera Caja
+    init_pair(5, COLOR_YELLOW, COLOR_BLUE);
+    attrset(COLOR_PAIR(5));
+    wattron(ventanaBox3, COLOR_PAIR(5)); 
     mvwprintw(ventanaBox3, 1, 1, "Patch CD Update");
+    wattroff(ventanaBox3, COLOR_PAIR(5));  
     mvwprintw(ventanaBox3, 2, 1, "Installation into Directory");
     mvwprintw(ventanaBox3, 3, 1, "Online Update");
     mvwprintw(ventanaBox3, 4, 1, "Install and Remove Software");
